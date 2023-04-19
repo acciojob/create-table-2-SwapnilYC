4b2b6ceb7be4f1f8100445b1e17c1c825f1f91bc
SwapnilYC <119727525+SwapnilYC@users.noreply.github.com>
2023-04-19 20:11:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 let tableContainer = document.getElementById('myTable');
 function createTable() {
     //Write your code here
-    let row = +(prompt('No of rows'))
-    let col = +(prompt('No of columns'))
+    let row = +(prompt('Input number of rows'))
+    let col = +(prompt('Input number of columns'))
 
     for (let i = 0; i < row; i++) {
         let newRow = tableContainer.insertRow(`${i}`);
         for (let j = 0; j < col; j++) {
             let newCell = newRow.insertCell(`${j}`);
-            newCell.innerHTML = `Row-${i} Column-${j + 1}`;
+            newCell.innerHTML = `Row-${i} Column-${j}`;
         }
     }
 }
