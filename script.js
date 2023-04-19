ebcb3b7e6400425626feafe069fd1a8be7b27056
SwapnilYC <119727525+SwapnilYC@users.noreply.github.com>
2023-04-19 20:08:10 +0530

    code added

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
+let tableContainer = document.getElementById('myTable');
 function createTable() {
     //Write your code here
-  
+    let row = +(prompt('No of rows'))
+    let col = +(prompt('No of columns'))
+
+    for (let i = 0; i < row; i++) {
+        let newRow = tableContainer.insertRow(`${i}`);
+        for (let j = 0; j < col; j++) {
+            let newCell = newRow.insertCell(`${j}`);
+            newCell.innerHTML = `Row-${i} Column-${j + 1}`;
+        }
+    }
 }
